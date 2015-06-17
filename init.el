c3d346cec1d069b05112089b8c526b02c98a6d67
Hao Wu <hwu@intellisurvey.com>
2015-06-17 13:45:32 -0700

    change color of linum

--- init.el
+++ init.el
@@ -381,3 +381,15 @@ If region is active, apply to active region instead."
 	 (apply #'max range)))
       (forward-line 1)
           (back-to-indentation)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(linum ((t (:background "black" :foreground "green")))))
