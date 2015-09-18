f2f5c0888dfb3829a322f42dc5332c6cd6349f4a
Hao Wu <hwu@intellisurvey.com>
2015-09-18 10:36:23 -0700

    tabbar

--- init.el
+++ init.el
@@ -96,6 +96,39 @@
 ;; Magit
 (global-set-key (kbd "C-x g") 'magit-status)
 
+;;;;; tabbar;;;;;;;;;;;;;;;;;;;;;;
+(require 'tabbar)
+; turn on the tabbar
+(tabbar-mode t)
+; define all tabs to be one of 3 possible groups: “Emacs Buffer”, “Dired”,
+;“User Buffer”.
+
+(defun tabbar-buffer-groups ()
+    "Return the list of group names the current buffer belongs to.
+This function is a custom function for tabbar-mode's tabbar-buffer-groups.
+This function group all buffers into 3 groups:
+Those Dired, those user buffer, and those emacs buffer.
+Emacs buffer are those starting with “*”."
+    (list
+     (cond
+      ((string-equal "*" (substring (buffer-name) 0 1))
+       "Emacs Buffer"
+       )
+      ((eq major-mode 'dired-mode)
+       "Dired"
+       )
+      (t
+       "User Buffer"
+       )
+      )))
+
+(setq tabbar-buffer-groups-function 'tabbar-buffer-groups)
+
+(global-set-key [M-s-left] 'tabbar-backward)
+(global-set-key [M-s-right] 'tabbar-forward)
+
+
+
 ;;;;;;;;;;;;;;;;; Programming Lang settings;;;;;;;;;;;
 
 
@@ -106,6 +139,11 @@
 (setq guru-warn-only t) ;; only show warn 
 
 (add-hook 'cperl-mode-hook (lambda () (flymake-mode t)))
+
+;; highlight
+(require 'auto-highlight-symbol)
+(global-auto-highlight-symbol-mode t)
+
 
 ;; JS
 (require 'js2-mode)
